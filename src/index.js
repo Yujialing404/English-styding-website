d9692756cfbdbd96616163385fe10668614adb54
yujialing <13281168701@163.com>
2019-11-23 12:51:36 +0800

    [修改]

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'antd/dist/antd.css';
 import { Layout } from 'antd';
+
 import './style.css';
+
 import PageList from './components/containers/List';
 import Detail from './components/containers/Detail';
 import Vip from './components/containers/Vip';
 import Login from './components/login/index';
-
 import AppHeader from './components/header/index';
 
 const { Header, Footer, Content } = Layout;
@@ -18,18 +19,18 @@ class App extends Component {
         return(
             <BrowserRouter>
                 <Layout style={{minWidth:1300}}>
-                <Header className='header'>
-                    <AppHeader />
-                </Header>
-                <Content className='content'>
-                    <Login />
-                    <Switch>
-                        <Route path='/vip' component={Vip}></Route>
-                        <Route path='/detail/:id' component={Detail}></Route>
-                        <Route path='/:id?' component={PageList}></Route>
-                    </Switch>
-                </Content>
-                <Footer className='footer'>Footer</Footer>
+                    <Header className='header'>
+                        <AppHeader />
+                    </Header>
+                    <Content className='content'>
+                        <Login />
+                        <Switch>
+                            <Route path='/vip' component={Vip}></Route>
+                            <Route path='/detail/:id' component={Detail}></Route>
+                            <Route path='/:id?' component={PageList}></Route>
+                        </Switch>
+                    </Content>
+                    <Footer className='footer'>Footer</Footer>
                 </Layout>
             </BrowserRouter>
         )
